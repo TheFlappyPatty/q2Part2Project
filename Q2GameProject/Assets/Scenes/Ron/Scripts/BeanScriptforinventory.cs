04f84ab4ae136da20cc26dbca0362b45facee9ec
MUKILTEO\1707223 <jrverburg@yahoo.com>
2021-12-09 12:31:38 -0800

    inventory system

---
+++ Q2GameProject/Assets/Scenes/Ron/Scripts/BeanScriptforinventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeanScriptforinventory : MonoBehaviour
+{
+    public int BeanCount;
+    public GameObject Bean;
+    public void addbean()
+    {
+        BeanCount++;
+        Instantiate(Bean).transform.localPosition = transform.localPosition;
+    }
+}
