c326bb5bc5b3a0c7fa1db035146a87e97f94aebc
MUKILTEO\1707228 <1707228@mukilteo.wednet.edu>
2021-12-14 12:48:12 -0800

    sdlkfj

---
+++ Q2GameProject/Assets/Scenes/Joseph/Scripts/OutOfBounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public GameObject spawnpoint;
+
+    public  void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.transform.position = spawnpoint.transform.position;
+    }
+
+
+}
