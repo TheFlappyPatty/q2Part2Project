9c771fb43340047a16dc1431b2a167f2c4f6bdff
MUKILTEO\1710219 <1710219@mulkilteo.wednet.edu>
2021-12-14 13:22:06 -0800

    end of day saving

--- Q2GameProject/Assets/Scenes/Adrian/Scripts/BeanSpawn.cs
+++ Q2GameProject/Assets/Scenes/Adrian/Scripts/BeanSpawn.cs
@@ -7,15 +7,32 @@ public class BeanSpawn : MonoBehaviour
 
     private Vector2 position1;
     private Vector2 position2;
+    public GameObject Beancan1;
+    public GameObject Beancan2;
+    public GameObject Beancan3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 4; i++)
+        {
+            if(i == 1)
+            {
+                Destroy(Beancan1); 
+            }
+            if (i == 2)
+            {
+                Destroy(Beancan2);
+            }
+            if (i == 3)
+            {
+                Destroy(Beancan3);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
