98b2612f5dc4d226c172bbdb5691d2b75c211d27
MUKILTEO\1710219 <1710219@mulkilteo.wednet.edu>
2021-12-14 13:06:59 -0800

    Saving work

--- Q2GameProject/Assets/Scenes/Adrian/Scripts/BeanSpawn.cs
+++ Q2GameProject/Assets/Scenes/Adrian/Scripts/BeanSpawn.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BeanSpawn : MonoBehaviour
 {
+
+    private Vector2 position1;
+    private Vector2 position2;
     // Start is called before the first frame update
     void Start()
     {
