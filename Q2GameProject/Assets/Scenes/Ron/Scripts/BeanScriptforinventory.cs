fee618cb142b07cb0eea44d67fd0c839886f0cd1
MUKILTEO\1707223 <jrverburg@yahoo.com>
2021-12-15 12:40:45 -0800

    Spawnworkdone

--- Q2GameProject/Assets/Scenes/Ron/Scripts/BeanScriptforinventory.cs
+++ Q2GameProject/Assets/Scenes/Ron/Scripts/BeanScriptforinventory.cs
@@ -6,16 +6,31 @@ using UnityEngine.UI;
 public class BeanScriptforinventory : MonoBehaviour
 {
     public  static int BeanCount;
+    public int beansspawned;
     public GameObject Bean;
     public GameObject spawnpoint;
     public Text Beancountshower;
     public void Update()
     {
         Beancountshower.text = ("" + BeanCount);
+        if(beansspawned >BeanCount)
+        {
+            spawnone();
+        }
     }
     public void addbean()
     {
         Instantiate(Bean,spawnpoint.transform.position,Quaternion.identity);
         BeanCount += 1;
+    }
+    public void Deathtoraccon()
+    {
+        BeanCount = 0;
+    }
+    public void spawnone()
+    {
+        Debug.Log("recovering");
+        Instantiate(Bean, spawnpoint.transform.position, Quaternion.identity);
+        beansspawned += 1;
     }
 }
