e8703c6ebc76e1a7b743b0538156f6862fb60d7e
MUKILTEO\1710219 <1710219@mulkilteo.wednet.edu>
2021-12-15 13:26:01 -0800

    working on boss level

--- Q2GameProject/Assets/Scenes/Adrian/Scripts/BeanSpawn.cs
+++ Q2GameProject/Assets/Scenes/Adrian/Scripts/BeanSpawn.cs
@@ -5,34 +5,101 @@ using UnityEngine;
 public class BeanSpawn : MonoBehaviour
 {
 
-    private Vector2 position1;
-    private Vector2 position2;
+    public Vector2 position1;
+    public Vector2 position2;
     public GameObject Beancan1;
     public GameObject Beancan2;
     public GameObject Beancan3;
+    private int i = 0;
+
     // Start is called before the first frame update
-    void Start()
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        for (int i = 0; i < 4; i++)
+        if (collision.gameObject.name == "bean1")
         {
-            if(i == 1)
-            {
-                Destroy(Beancan1); 
-            }
-            if (i == 2)
-            {
-                Destroy(Beancan2);
-            }
-            if (i == 3)
-            {
-                Destroy(Beancan3);
-            }
+            Destroy(Beancan1);
+            StartCoroutine(can1());
+        }
+
+        if (collision.gameObject.name == "bean2")
+        {
+            Destroy(Beancan2);
+            StartCoroutine(can2());
+            
+            
+        }
+
+        if (collision.gameObject.name == "bean3")
+        {
+            Destroy(Beancan3);
+            StartCoroutine(endGame());
         }
     }
+   
 
-    // Update is called once per frame
-    void Update()
+
+    public IEnumerator can1()
     {
-       
+
+
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            
+            Beancan2.GetComponent<SpriteRenderer>().enabled = true;
+            Beancan2.GetComponent<BoxCollider2D>().enabled = true;
+            
+            break;
+        }
+
+        
+    }public IEnumerator can2()
+    {
+        Debug.Log("Started");
+
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            
+            Beancan3.GetComponent<SpriteRenderer>().enabled = true;
+            Beancan3.GetComponent<BoxCollider2D>().enabled = true;
+            
+            break;
+        }
+
+        StopCoroutine(can2());
     }
+    //public IEnumerator can2()
+    //{
+
+
+    //    yield return new WaitForSeconds(5);
+    //    while (true)
+    //    {
+            
+    //        Beancan3.GetComponent<SpriteRenderer>().enabled = true;
+    //        Beancan3.GetComponent<BoxCollider2D>().enabled = true;
+
+    //        break;
+    //    }
+
+    //    StopCoroutine(can2());
+    //}
+
+    public IEnumerator endGame()
+    {
+
+
+        yield return new WaitForSeconds(5);
+        
+
+        StopCoroutine(endGame());
+    }
+
+
+
+
 }
