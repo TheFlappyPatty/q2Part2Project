172c1c0f0950751de804ded73d579e0e7ca45b8c
MUKILTEO\1707223 <jrverburg@yahoo.com>
2022-01-12 12:32:36 -0800

    ye

--- Q2GameProject/Assets/Scenes/Joseph/Scripts/OutOfBounds.cs
+++ Q2GameProject/Assets/Scenes/Joseph/Scripts/OutOfBounds.cs
@@ -6,13 +6,16 @@ public class OutOfBounds : MonoBehaviour
 {
     public GameObject spawnpoint;
     public GameObject Beaninventory;
+    public void Start()
+    {
+       Beaninventory = GameObject.Find("Inventory");
+    }
 
     public  void OnCollisionEnter2D(Collision2D collision)
     {
         collision.gameObject.transform.position = spawnpoint.transform.position;
 
-        Beaninventory.GetComponent<BeanScriptforinventory>().Deathtoraccon();
-  //origin/ParticalFinish
+       Beaninventory.GetComponent<BeanScriptforinventory>().Deathtoraccon();
     }
 
 
